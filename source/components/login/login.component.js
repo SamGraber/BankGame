4c23818b8dc6cd9707724af8c8189eb3d49c86b1
SamGraber <samgraber@sundialproperties.com>
2016-03-24 18:48:31 -0400

    Build

--- source/components/login/login.component.js
+++ source/components/login/login.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', '../../services/authentication/authentication.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/common', '../../services/authentication/authentication.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,12 +8,15 @@ System.register(['angular2/core', '../../services/authentication/authentication.
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, authentication_service_1;
+    var core_1, common_1, authentication_service_1;
     var LoginComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             },
             function (authentication_service_1_1) {
                 authentication_service_1 = authentication_service_1_1;
@@ -22,13 +25,15 @@ System.register(['angular2/core', '../../services/authentication/authentication.
             LoginComponent = (function () {
                 function LoginComponent(authenticationService) {
                     this.authenticationService = authenticationService;
+                    this.model = {};
                 }
                 LoginComponent.prototype.onSubmit = function () {
-                    console.log('Submitted');
+                    this.authenticationService.login(this.model);
                 };
                 LoginComponent = __decorate([
                     core_1.Component({
                         templateUrl: 'source/components/login/login.component.html',
+                        directives: [common_1.FORM_DIRECTIVES],
                     }), 
                     __metadata('design:paramtypes', [authentication_service_1.AuthenticationService])
                 ], LoginComponent);
