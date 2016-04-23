5d68eed1683a76b366b257ede5d83689f2cd75aa
SamGraber <samgraber@sundialproperties.com>
2016-04-23 14:27:17 -0400

    Conditionally show 'Make a withdrawal' or 'Request a loan' based on the user's current balance.

--- source/components/account/account.component.ts
+++ source/components/account/account.component.ts
@@ -1,10 +1,12 @@
 import { Component } from 'angular2/core';
+import { COMMON_DIRECTIVES } from 'angular2/common';
 import { Router } from 'angular2/router';
 import { AccountService, IAccount } from '../../services/account/account.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 @Component({
     templateUrl: 'source/components/account/account.component.html',
+	directives: [COMMON_DIRECTIVES],
 })
 export class AccountComponent {
 	account: IAccount;
