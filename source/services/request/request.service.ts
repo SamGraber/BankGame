ec993eaf1aa15fa634e70d99afbfa28af5e8db17
SamGraber <samgraber@sundialproperties.com>
2016-04-23 14:59:48 -0400

    Use spaces.

--- source/services/request/request.service.ts
+++ source/services/request/request.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from 'angular2/core';
-import {Http, Response, Headers, RequestOptions} from 'angular2/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from 'angular2/core';
+import { Http, Response, Headers, RequestOptions } from 'angular2/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RequestService {
