47fb2e8f035060364e2258c233a27684c449cbf1
SamGraber <samgraber@sundialproperties.com>
2016-04-23 09:31:23 -0400

    Use local storage to manage user sessions.

--- source/services/authentication/authentication.service.ts
+++ source/services/authentication/authentication.service.ts
@@ -2,6 +2,12 @@ import {Injectable} from 'angular2/core';
 import {Observable} from 'rxjs/observable';
 import { RequestService } from '../request/request.service';
 
+declare var localStorage: ISessionStorage;
+
+interface ISessionStorage {
+	loggedInUser: string;
+}
+
 export interface IUser {
 	username: string;
 	accountId: number;
@@ -19,14 +25,28 @@ export class AuthenticationService {
 	
 	constructor(private http: RequestService) {}
 	
+	restoreSession(): boolean {
+		if (localStorage.loggedInUser) {
+			this.loggedInUser = JSON.parse(localStorage.loggedInUser);
+			this.isAuthenticated = true;
+			return true;
+		}
+		return false;
+	}
+	
 	login(credentials: ICredentials): Observable<IUser> {
 		return this.http.post('/users/login', credentials)
 					.map(user =>  {
 						this.loggedInUser = user;
 						console.log(user.username + ' is now logged in');
+						localStorage.loggedInUser = JSON.stringify(user);
 						this.isAuthenticated = true;
 						return this.loggedInUser;
 					});
+	}
+	
+	logout(): void {
+		localStorage.loggedInUser = null;
 	}
 	
 	register(credentials: ICredentials): Observable<IUser> {
