59b6928ac16732f13fab191f78696368a50b7d10
SamGraber <samgraber@sundialproperties.com>
2016-04-22 21:49:55 -0400

    Build

--- source/app.component.js
+++ source/app.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/router', './components/login/login.component', './components/main/main.component', './services/authentication/authentication.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './components/login/login.component', './components/register/register.component', './components/main/main.component', './services/authentication/authentication.service', './services/request/request.service'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +10,7 @@ System.register(['angular2/core', 'angular2/router', './components/login/login.c
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1, login_component_1, main_component_1, authentication_service_1;
+    var core_1, router_1, login_component_1, register_component_1, main_component_1, authentication_service_1, request_service_1;
     var AppComponent;
     return {
         setters:[
@@ -21,11 +23,17 @@ System.register(['angular2/core', 'angular2/router', './components/login/login.c
             function (login_component_1_1) {
                 login_component_1 = login_component_1_1;
             },
+            function (register_component_1_1) {
+                register_component_1 = register_component_1_1;
+            },
             function (main_component_1_1) {
                 main_component_1 = main_component_1_1;
             },
             function (authentication_service_1_1) {
                 authentication_service_1 = authentication_service_1_1;
+            },
+            function (request_service_1_1) {
+                request_service_1 = request_service_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -35,12 +43,13 @@ System.register(['angular2/core', 'angular2/router', './components/login/login.c
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n\t\t<h1>Bank Game</h1>\n\t\t<ul class=\"nav nav-tabs\">\n\t\t\t<li role=\"presentation\" *ngIf=\"authentication.isAuthenticated\"><a [routerLink]=\"['Main']\">Main</a></li>\n\t\t\t<li role=\"presentation\"><a [routerLink]=\"['Login']\">Login</a></li>\n\t\t</ul>\n\t\t<router-outlet></router-outlet>\n\t",
+                        template: "\n\t\t<h1>Bank Game</h1>\n\t\t<ul class=\"nav nav-tabs\" *ngIf=\"authentication.isAuthenticated\">\n\t\t\t<li role=\"presentation\"><a [routerLink]=\"['Main']\">Main</a></li>\n\t\t</ul>\n\t\t<ul class=\"nav nav-tabs\" *ngIf=\"!authentication.isAuthenticated\">\n\t\t\t<li role=\"presentation\"><a [routerLink]=\"['Login']\">Login</a></li>\n\t\t\t<li role=\"presentation\"><a [routerLink]=\"['Register']\">Register</a></li>\n\t\t</ul>\n\t\t<router-outlet></router-outlet>\n\t",
                         directives: [router_1.ROUTER_DIRECTIVES],
-                        providers: [authentication_service_1.AuthenticationService],
+                        providers: [authentication_service_1.AuthenticationService, request_service_1.RequestService],
                     }),
                     router_1.RouteConfig([
                         { path: '/login', name: 'Login', component: login_component_1.LoginComponent },
+                        { path: '/register', name: 'Register', component: register_component_1.RegisterComponent },
                         //   {path:'/user',  name: 'Profile', component: HeroListComponent},
                         //   {path:'/bank',  name: 'Bank',    component: HeroDetailComponent}
                         { path: '/main', name: 'Main', component: main_component_1.MainComponent }
@@ -48,7 +57,7 @@ System.register(['angular2/core', 'angular2/router', './components/login/login.c
                     __metadata('design:paramtypes', [authentication_service_1.AuthenticationService])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
--- source/services/authentication/authentication.service.js
+++ source/services/authentication/authentication.service.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/http'], function(exports_1) {
+System.register(['angular2/core', '../request/request.service'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,15 +10,15 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1;
+    var core_1, request_service_1;
     var AuthenticationService;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (http_1_1) {
-                http_1 = http_1_1;
+            function (request_service_1_1) {
+                request_service_1 = request_service_1_1;
             }],
         execute: function() {
             AuthenticationService = (function () {
@@ -26,25 +28,29 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1) {
                 }
                 AuthenticationService.prototype.login = function (credentials) {
                     var _this = this;
-                    console.log(credentials.username + ' is now logged in');
-                    this.isAuthenticated = true;
-                    var body = JSON.stringify({ name: name });
-                    var headers = new http_1.Headers({ 'Content-Type': 'application/json' });
-                    var options = new http_1.RequestOptions({ headers: headers });
-                    return this.http.post('/users/login', body, options)
-                        .map(function (res) {
-                        _this.loggedInUser = res.json().data;
+                    return this.http.post('/users/login', credentials)
+                        .map(function (user) {
+                        _this.loggedInUser = user;
+                        console.log(user.username + ' is now logged in');
+                        _this.isAuthenticated = true;
                         return _this.loggedInUser;
-                    })
-                        .catch(function (err) { return console.error(err); });
+                    });
+                };
+                AuthenticationService.prototype.register = function (credentials) {
+                    var _this = this;
+                    return this.http.post('/users/register', credentials)
+                        .map(function (user) {
+                        _this.loggedInUser = user;
+                        _this.isAuthenticated = true;
+                        return _this.loggedInUser;
+                    });
                 };
                 AuthenticationService = __decorate([
                     core_1.Injectable(), 
-                    __metadata('design:paramtypes', [(typeof (_a = typeof http_1.Http !== 'undefined' && http_1.Http) === 'function' && _a) || Object])
+                    __metadata('design:paramtypes', [request_service_1.RequestService])
                 ], AuthenticationService);
                 return AuthenticationService;
-                var _a;
-            })();
+            }());
             exports_1("AuthenticationService", AuthenticationService);
         }
     }
