156385df870c49adb23412b190db47d653738972
SamGraber <samgraber@sundialproperties.com>
2016-04-23 15:01:04 -0400

    Use spaces

--- source/main.ts
+++ source/main.ts
@@ -2,7 +2,7 @@ import 'rxjs/Rx';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import { bootstrap }    from 'angular2/platform/browser';
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
