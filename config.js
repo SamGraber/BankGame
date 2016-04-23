141f8cf82947fdeaab7adbf11f99f6fe4e8ff784
SamGraber <samgraber@sundialproperties.com>
2016-04-23 15:04:54 -0400

    Configure lodash with systemjs.

--- config.js
+++ config.js
@@ -6,10 +6,14 @@ System.config({
     "source": {
       "format": "register",
       "defaultExtension": "js"
+    },
+    "lodash": {
+      "main": "lodash.js",
     }
   },
   map: {
     "angular2": "node_modules/angular2",
     "rxjs": "node_modules/rxjs",
+    "lodash": "node_modules/lodash",
   }
 });
