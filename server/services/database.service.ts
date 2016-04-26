df6639f8a0d6ef7e243cd6429031e0925b12cdc2
SamGraber <samgraber@sundialproperties.com>
2016-04-26 19:33:37 -0400

    Implemented update.

--- server/services/database.service.ts
+++ server/services/database.service.ts
@@ -9,6 +9,20 @@ export class DatabaseService {
 	constructor(public database: any) { }
 
 	update(model: IModel): Promise<IModel> {
-		return null;
+		return new Promise((resolve: Function, reject: Function): void => {
+			this.database.update({ '_id': '1' }, this.buildUpdateModel(model), (updateError): void => {
+				this.database.findOne({ '_id': '1' }, (findError, updatedModel): void => {
+					resolve(updatedModel);
+				});
+			});
+		});
+	}
+
+	private buildUpdateModel(model: IModel): any {
+		const updateModel: any = { $set: {} };
+		_.each(model.properties, (prop: string): any => {
+			updateModel.$set[prop] = model[prop];
+		});
+		return updateModel;
 	}
 }
