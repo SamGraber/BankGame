6ef0ab5a07acc456446fc609ba8da272543cdbc0
SamGraber <samgraber@sundialproperties.com>
2016-04-26 18:38:16 -0400

    Added a karma config with karma-systemjs

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+module.exports = function(config) {
+	config.set({
+
+		basePath: './',
+
+		files: [
+			'source/*tests.js',
+			'source/**/*tests.js',
+			'server/**/*tests.js',
+		],
+
+		systemjs: {
+			// Point out where the SystemJS config file is
+			configFile: 'config.js',
+
+			serveFiles: [
+				'source/*.js',
+				'source/**/*.js',
+				'server/**/*.js',
+			],
+
+			// Add any additional configuration, such as mappings to modules only used in testing
+			config: {
+				paths: {
+				}
+			}
+		},
+
+		autoWatch: true,
+
+		frameworks: ['systemjs', 'mocha', 'chai'],
+
+		browsers: ['Chrome'],
+
+		plugins: [
+			'karma-systemjs',
+			'karma-chrome-launcher',
+			'karma-firefox-launcher',
+			'karma-mocha',
+			'karma-chai',
+		],
+
+	});
+};
